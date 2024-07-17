520720bded12771e0b87e7975a093c090ac827fe
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-17 10:49:34 +0530

    Add files via upload

---
+++ sec convert hr.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int sec ,hr, min ,s;
+	{
+		printf("enter a sec ");
+		scanf("%d",&sec);
+		
+		hr = s/3600;
+		s= s%3600;
+		min= s/60;
+		s=s%60;
+		
+		printf("%d",se)
+	
+	}
+	return 0;
+}
