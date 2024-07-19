dd3a066bcec386997541bf714a9f02d31f1db1a8
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-19 09:39:05 +0530

    Add files via upload

---
+++ display series.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int num ,i,sum=0;
+	
+	printf("enter the num");
+	scanf("%d",&num);
+	
+	printf(" 1+ ");
+	
+	for(i=2;i<=num-1;i++)
+	{
+		printf("1/%d",i);
+		
+	}
+	
+	for (i=0;i<=num;i++)
+	{
+		sum=+ num;
+		
+		
+	}
+	printf("1/%d",num);
+	printf("1/%d",sum+1/num);
+	
+	return 0;
+}
