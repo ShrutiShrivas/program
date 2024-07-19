79575a9805a2c9dd7e795b30bb864620f1016b0d
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-19 17:37:08 +0530

    Add files via upload

---
+++ absfun.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#define N 10
+int abs_val(int);
+
+int main() {
+    int x, result;
+
+    
+    printf("Input a value (negative): \n");
+    scanf("%d", &x);
+    printf("Original value = %d", x);
+
+ 
+    result = abs_val(x);
+
+    
+    printf("\nAbsolute value = %d", result);
+
+    return 0;
+}
+
+
+int abs_val(int y) {
+    if(y < 0)
+        return(y * -1);
+    else
+        return(y);
+}
