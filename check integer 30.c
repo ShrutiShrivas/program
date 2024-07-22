a795bd2dbd8c476804bd75f51d1e9432471c0626
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-22 15:09:53 +0530

    Add files via upload

---
+++ check integer 30.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int x,y;
+	printf("number x value");
+	scanf("%d",&x);
+	printf("number y value");
+	scanf("%d",&y);
+	
+	if(x==30||y==30||(x+y==30))
+	{
+		printf("true");
+	}
+	else{
+			
+	printf("false");
+	}
+	return 0;
+	}
