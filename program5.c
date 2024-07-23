f6d95bade1dbb25a9e0d85037cb114a7d7f430c6
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-23 19:04:29 +0530

    Add files via upload
    Write a C program that checks if a given non-negative number is a multiple of 3 or 7, but not both.

---
+++ program5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int x,y;
+	printf("number x value");
+	scanf("%d",&x);
+	
+	if(x%3==0||x%7==0)
+		{
+		printf("true");
+	}
+	else{
+			
+	printf("false");
+	}
+	return 0;
+	}
