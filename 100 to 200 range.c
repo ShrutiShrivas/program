70262bbb4384e3aa713ab950357868d4cc4f9c86
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-23 10:18:03 +0530

    Add files via upload

---
+++ 100 to 200 range.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int x,y;
+	printf("number x value");
+	scanf("%d",&x);
+	printf("number y value");
+	scanf("%d",&y);
+	
+	if((x>=100&&x<=200)||(y>=100&&y<=200))
+	{
+		printf("true");
+	}
+	else{
+			
+	printf("false");
+	}
+	return 0;
+	}
