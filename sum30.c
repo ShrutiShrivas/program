974310f97d44da86c8a623a5971fde592ccc8dc3
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-23 11:59:34 +0530

    Add files via upload

---
+++ sum30.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ int main()
+ {
+ 	int x,y;
+ 	printf("x");
+ 	scanf("%d",&x);
+ 	
+ 	printf("y");
+ 	scanf("%d",&y);
+ 	if(x+y==30)
+ 	{
+ 		printf("true");
+ 		
+	 }
+	 else
+	 {
+	 	printf("false");
+	 }
+	 return 0;
+ 	
+ }
