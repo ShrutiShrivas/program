8d8baf7f4be758aea41b8767b1376e6398865e85
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-23 16:37:37 +0530

    Add files via upload
    Array of sum.

---
+++ sumofarray.c.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a[5],i ;
+	printf("enter array element");
+	
+	for(i=0;i<=4;i++)
+	
+		scanf("%d",&a[i]);
+	
+	printf("\n reverse element");
+	
+	for(i=4;i>=0;i--)
+	
+		scanf("%d",&a[i]);
+	
+	return 0;
+	
+}
