cafeddb5b19f725cbf4bd8401d1688941debc6cf
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-23 21:49:47 +0530

    Update program1.c
    Write a C program to find the larger of two given integers. However if the two integers have the same remainder when divided by 5, choose the smaller integer. If the two integers are the same, return 0.

--- program1.c.cpp
+++ program1.c.cpp
@@ -1,28 +1,25 @@
 #include<stdio.h>
 int main()
 {
-	int num1,num2,rem1,rem2;
+	int num1,num2;
 		
  	
  	printf("num1");
  	scanf("%d",&num1);
- 		
- 	
  	printf("num2");
  	scanf("%d",&num2);
- 	rem1=num1%5;
- 	rem2=num2%5;
  	
- 	if(rem1==rem2)
- 	{
- 		printf("%d",num1>num2,num2);
- 		printf("%d",num2>num1,num1);
- 		
-	 }
-	 else
-	 {
-	 	printf("%d",num1==num2);
-	 
-	 }
-	 return 0;
+ 	
+    if (num1 == num2) 
+    {
+      printf("0");
+    }
+    else if ((num1 % 5 == num2 % 5 && num1 < num2) || num1 > num2) 
+    {
+        printf("%d",num1);
+    }
+    else
+    {
+        printf("%d",num2);
+    }
 }
