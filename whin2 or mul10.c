caa5a7f628cb1ea3d391073a7516b931e955f114
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-23 11:59:11 +0530

    Add files via upload

---
+++ whin2 or mul10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ int main()
+ {
+ 	int x,y;
+ 	printf("x");
+ 	scanf("%d",&x);
+ 	
+ 
+ 	if((x<=2)||(x%10==0))
+ 	{
+ 			printf("true");
+ 		
+	 }
+	 else
+	 {
+	 	printf("false");
+	 }
+	 return 0;
+ 	
+	 }
