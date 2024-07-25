314a45d43d82d5a3efca6d576a3497e0ac59a241
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-25 15:40:56 +0530

    Add files via upload
    Write a C program to check whether an array of integers with a length of 2 contains 15 or 20

---
+++ program7.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+
+void mid_array(int parray[], int size);
+
+int main(void){ 
+    
+    int arr_size;
+    int a1[] = {10, 20, -30, -40, 30 };
+    int a2[] = {10, 20, 30, 40, 30};
+    int arr_size1 = sizeof(a1)/sizeof(a1[0]);
+    int arr_size2 = sizeof(a2)/sizeof(a2[0]);
+
+    
+  
+    print_array(a1, arr_size1);
+    print_array(a2, arr_size2);  
+
+    
+    int result[] = { a1[2], a2[2]  };
+
+    
+    arr_size = sizeof(result)/sizeof(result[0]);
+
+    
+    printf("Elements in new array are: ");  
+    print_array(result, arr_size);
+}     
+
+
+void print_array(int parray[], int size)
+{
+    int i;      
+    for( i=0; i<size-1; i++)  
+    {  
+        
+        printf("%d, ", parray[i]);  
+    } 
+    
+    printf("%d ", parray[i]);  
+   
+    printf("\n");   
+}
+
