3951d87a52112297fb44499947cbabda6cffbe85
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-28 16:23:18 +0530

    Add files via upload

---
+++ program14.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+int arr1[] ={1,5,7};
+int arraysize,i,result;
+
+arraysize=sizeof(arr1)/sizeof(arr1[0]);
+
+
+
+
+
+
+		for(i=0;i<arraysize-1;i++)
+	{
+		if(arr1[i]==5 && arr1[i+1]==7)
+		
+		
+		result=arr1[i+1]==1;
+		
+		printf("%d ",result[arr1]);
+		
+	
+	}
+
+	
+	
+	
+}
