4ad7f43b87d9dbf3e168f71f5a691ea26a6edfe9
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-29 23:09:49 +0530

    Add files via upload
    Largest element.

---
+++ Program20.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() 
+{
+  int n;
+  int arr[10];
+  printf("Enter the number of elements : ");
+  scanf("%d", &n);
+
+  for (int i = 0; i < n; i++) {
+    printf("Enter number%d: ", i );
+    scanf("%d", &arr[i]);
+  }
+
+  
+  for (int i = 1; i < n; i++) {
+    if (arr[0] < arr[i]) {
+      arr[0] = arr[i];
+    }
+  }
+
+  printf("Largest element = %d", arr[0]);
+
+  return 0;
+}
