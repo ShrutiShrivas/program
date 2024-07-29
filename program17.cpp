440e72c92f2cf3322beaa4cce4012217bc6f031f
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-07-29 17:01:21 +0530

    Add files via upload
    Write a C program to create an array taking two middle elements from a given array of integers of length even.

---
+++ program17.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	int arr1[]={6,2,3,8,4,7};
+	
+	
+
+	int arrsize;
+	
+	arrsize=sizeof(arr1)/sizeof(arr1[0]);
+//	printf("%d",arrsize);
+	
+//	arrsize2=sizeof(arr2)/sizeof(arr2[0]);
+//	printf("\n%d",arrsize);
+
+//int mid=arrsize/2-1;
+int mid1=arrsize/2;
+//printf("%d",arr1[mid]);
+printf("%d",arr1[mid1]);
+
+		
+}
