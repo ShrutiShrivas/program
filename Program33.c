b7e748ed5b7020becbe0063775207e037c6768e6
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-08-02 12:38:52 +0530

    Update Program33.c

--- Program33.c
+++ Program33.c
@@ -1,42 +1,25 @@
-#include<stdio.h>
-int main()
-{
-	int arr[5]={1,2,3,0,0};
-	int temp[5];
-	int index;
-	int i;
-	
-	for(i=0;i<5;i++)
-	{
-		temp[i]=arr[i];
-		printf("%d",arr[i]);
-	}
-	
-	for(i=0;i<5;i++)
-	{
-		if(temp[i]==0 )
-		{
-		arr[index]=temp[i];
-		index++;
-		}
-	
-	}
-		
-		for(i=0;i<5;i++)
-	{
-		if(temp[i]!=0 )
-		{
-		arr[index]=temp[i];
-		index;
-		}
-	
-	}	
-		
-
-	for(i=0;i<5;i++)
-	{
-		printf("%d\n",arr[i]);
-	}
-	return 0;
-	
+#include <stdio.h> 
+int main() 
+{ 
+    int arr[] = { 5, 6, 0, 4, 6, 0, 9, 0, 8 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    int j = 0; 
+    int i;
+    for ( i = 0; i < n; i++)
+	 { 
+        if (arr[i] == 0) 
+		{ 
+            int temp = arr[i]; 
+            arr[i] = arr[j]; 
+            arr[j] = temp; 
+            j++; 
+        } 
+    } 
+    for (i = 0; i < n; i++)
+	{ 
+        printf("%d ", arr[i]);
+        
+    } 
+  
+    return 0; 
 }
