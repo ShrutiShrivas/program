174eb80afd926172f4878995c95b71eaf83754c9
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-08-02 12:26:38 +0530

    Add files via upload
    Check age you can vote or not.

---
+++ checkage1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int age =18;
+	
+	printf("enter the age");
+	scanf("%d",&age);
+
+	
+	if(age>=18)
+	{
+		printf("You can vote");
+	}
+	else
+	
+	{
+		printf("your age is less than 18");
+	}
+	return 0;
+}
+
