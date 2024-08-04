9c11275a59c08451d74dc2edc1cffb552c4c3503
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-08-04 15:59:56 +0530

    Add files via upload

---
+++ m value check.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int m,n;
+	printf("enter m value");
+	scanf("%d",&m);
+	if(m!=0)
+	{
+		
+		if(m>0)
+		n=1;
+		else
+		n=-1;
+	}
+	printf("the value of m %d\n",m);
+	printf("the value of n %d\n",n);
+}
