e0d15c0bdc62328a7bcf625910a27162e9ccaf50
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-08-09 15:26:57 +0530

    Add files via upload

---
+++ pattern4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+
+int r,c;
+
+for(r=1;r<=4;r++)
+{
+	for(c=1;c<=4;c++)
+	{
+		if(r%2==0)
+		
+			{
+			printf("A");
+			}
+			else
+		{
+			printf("%d",c);
+		}	
+	}
+	
+	printf("\n");	
+}	
+
+}
+
