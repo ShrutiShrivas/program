52308a4f128c42fce47848de41327b7ceff6eba7
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-08-21 14:07:01 +0530

    Add files via upload

---
+++ rev logic string.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	char str[6];
+	int i,j,l=0;
+	
+	printf("Enter the string");
+	gets(str);
+	
+	for(i=0;str[i]!='\0';i++)
+	{
+		l=l+1;
+	}
+	printf("%d\n",l);
+	
+	for(j=l-1;j>=0;j--)
+	{
+		printf("%c",str[j]);
+	}
+}
