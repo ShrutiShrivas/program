20913fcd9511e6c75a30e942975f14d68afe6a26
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-08-26 12:50:16 +0530

    Add files via upload

---
+++ pattern36.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	
+	for(i='E';i>='A';i--)
+	{
+		for(j='A';j<=i;j++)
+		{
+			printf(" ");
+		}
+		for(j='A';j<=i;j++)
+		{
+			printf("%c",j);
+		}
+		printf("\n");
+	}
+}
