6d3de40ebe87ae42cc6fbb804b40d5cb9178a989
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-08-27 14:58:42 +0530

    Add files via upload

---
+++ pattern45.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	 int i,j,k,p='@';
+	 
+	  for(i=1;i<=4;i++)
+  	{
+  		k=1;
+  		p++;
+  			for(j=1;j<=7;j++)
+  		{
+  			if(j>=5-i && j<=3+i &&k==1)
+  			{
+  			printf("%c",p);
+  			k=0;
+			}
+			else
+			{
+			printf(" ");
+			k=1;
+			}
+		}
+	 printf("\n");
+	}
+}
+   
+  
+  
+    
+  
+	
+	
+
