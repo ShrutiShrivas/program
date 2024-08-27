70089b5dde1f2dc3e783ecefd17e95f7ef592aea
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-08-27 14:55:11 +0530

    Add files via upload

---
+++ arthmaticwith fun.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void sum()
+{
+	int a=20,b=10;	
+
+	printf("Addtion %d\n",a+b);
+}
+void sub()
+{
+	int a=20,b=10;	
+
+	printf("Subtration %d\n",a-b);
+}
+void mul()
+{
+	int a=20,b=10;	
+
+	printf("Multiplication %d\n",a*b);
+}
+void div()
+{
+	int a=20,b=10;	
+
+	printf("Division of %d\n",a/b);
+}
+
+void main()
+{
+	div();
+	mul();
+	sub();
+	sum();
+	
+}
+
