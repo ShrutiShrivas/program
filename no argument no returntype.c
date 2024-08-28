30d263a714db2c44bd6f073d93376f31e0e4883e
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-08-28 15:47:40 +0530

    Add files via upload

---
+++ no argument no returntype.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void areaofrec()
+{
+	int l,b,res;
+	printf("Enter the length breath");
+	scanf("%d %d",&l,&b);
+	
+	res=l*b;
+	printf("%d",res);
+}
+void main()
+{
+	areaofrec();
+}
