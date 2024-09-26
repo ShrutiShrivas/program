ef0c03cd38fa29462aceb7fc9f3080d433040765
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-09-26 15:13:21 +0530

    Add files via upload

---
+++ static_function.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+	static int a;
+class one
+{
+public:
+	int b;
+	
+static void set()
+{
+	a++;
+}
+void dis()
+{
+	cout<<a<<endl;
+}
+};
+int main()
+{
+	one obj1,obj2,obj3;
+	obj1.dis();
+	obj2.dis();
+	obj3.dis();
+	
+	one::set();
+	obj1.dis();
+	obj2.dis();
+	obj3.dis();
+	
+	
+}
